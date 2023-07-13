41faf1e779755822eb339cec2c5baf259c5ac42a
Fernando Almeida <tinhofernando44@gmail.com>
2023-07-13 17:08:05 -0300

    novo 2

--- 2/cnpq_id_2.py
+++ 2/cnpq_id_2.py
@@ -116,6 +116,10 @@ class Info:
             sleep(3)
             self.driver.find_element('xpath', '/html/body/form/div/div[4]/div/div/div/div[3]/div/div[2]/a['+ str(p + 1) +']').click() # page: 2100
             sleep(3)
+            self.driver.find_element('xpath', '/html/body/form/div/div[4]/div/div/div/div[3]/div/div[2]/a['+ str(p + 1) +']').click() # page: 2100
+            sleep(3)
+            self.driver.find_element('xpath', '/html/body/form/div/div[4]/div/div/div/div[3]/div/div[2]/a['+ str(p + 1) +']').click() # page: 2100
+            sleep(3)
             
             for i in range(2, 12):
                 #p = 6
@@ -179,7 +183,7 @@ class Info:
         df = pd.DataFrame(ids)
 
         # Escrever o dataframe em um arquivo CSV
-        df.to_csv('nova_coleta/ids2_page_25.csv') # 
+        df.to_csv('nova_coleta/ids2_page_27.csv') # 
         #self.getInfosPubl
     '''def getInfosPublish(self):
         Bs = bs(self.driver.page_source, 'html.parser')
